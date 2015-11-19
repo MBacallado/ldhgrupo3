25dbb759c0bcbb45fedc1ba015f5d49b1c8fc2f8
SerSantin <sergio.ms.91@gmail.com>
2015-11-19 23:52:14 +0000

    "Modificado ProximidadActivity, creando la instancia del sensor proximidad"

--- app/src/main/java/com/example/manuelalejandro/ejemplo1/ProximidadActivity.java
+++ app/src/main/java/com/example/manuelalejandro/ejemplo1/ProximidadActivity.java
@@ -2,22 +2,57 @@ package com.example.manuelalejandro.ejemplo1;
 
 import android.app.Activity;
 import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import java.util.List;
 
 /**
  * Created by ManuelAlejandro on 16/11/2015.
  */
-public class ProximidadActivity extends Activity{
+public class ProximidadActivity extends Activity implements SensorEventListener{
+    LinearLayout fondo;
+    Sensor s;
+    SensorManager sensorM;
+    List<Sensor> sensores;
+
+    /**
+     *       Primero se le especifica a la clase MainActivity que implemente el SensorEventListener, esto
+     *  se hace para manipular los eventos del sensor y ejecutar nuestro código cuando cambie.
+     *  @param savedInstanceState
+     *
+     */
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_proximidad);
+        setContentView(R.layout.activity_main);
+        fondo = (LinearLayout) findViewById(R.id.fondo);
+
+        sensorM = (SensorManager) getSystemService(SENSOR_SERVICE);
+        sensores = sensorM.getSensorList(Sensor.TYPE_PROXIMITY);
+        if (!sensores.isEmpty()){
+            s = sensores.get(0);
+            sensorM.registerListener(this,s,sensorM.SENSOR_DELAY_UI);
+        }
+    }
+
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }
