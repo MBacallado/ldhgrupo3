09a31c123fdd85aae867e53d3ebff31cc994d0ba
mamu21 <manuelbacallado89@gmail.com>
2015-11-19 23:34:30 +0000

    "Main activity completado y comentado"

--- app/src/main/java/com/example/manuelalejandro/ejemplo1/MainActivity.java
+++ app/src/main/java/com/example/manuelalejandro/ejemplo1/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.manuelalejandro.ejemplo1;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,9 +9,15 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button botonProximidad;
 
+    /**
+     * Constructor de la clase Activity,
+     * @param savedInstanceState guarda una instancia de la actividad pasada por parametro
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,27 +33,53 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        /**
+         * Se declara boton para el sensor proximidad
+         */
+        botonProximidad = (Button) findViewById(R.id.button);
+        botonProximidad.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                proximidad(v);
+            }
+        });
+
     }
 
+    /**
+     * Añade elementos a la barra de acción si está presente
+     * @param menu
+     * @return true
+     */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
+    /**
+     * Coge el id , comprueba si estaá en los ajustes, si está return true, si no, retorna el item seleccionado en las opciones
+     * @param item
+     * @return
+     */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * Función que crea el intent con la actividad y la inicia.
+     * @param view
+     */
+    public void proximidad(View view) {
+        Intent i = new Intent(this, ProximidadActivity.class);
+        startActivity(i);
     }
 }
--- app/src/main/java/com/example/manuelalejandro/ejemplo1/ProximidadActivity.java
+++ app/src/main/java/com/example/manuelalejandro/ejemplo1/ProximidadActivity.java
@@ -1,14 +1,19 @@
 package com.example.manuelalejandro.ejemplo1;
 
 import android.app.Activity;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.TextView;
 
 /**
  * Created by ManuelAlejandro on 16/11/2015.
  */
 public class ProximidadActivity extends Activity{
-    private TextView salida;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
